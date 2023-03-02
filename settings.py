5e7625b849fbf183aca1f890352080fc562cd5c7
tomfo0lery <59426914+tomfo0lery@users.noreply.github.com>
2023-03-02 12:45:28 -0500

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+# settings.py - The settings module for alien_invasion
+
+class Settings:
+    """A class for storing all the settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
